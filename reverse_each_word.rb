145533825166a7b474a577c636e296a2f491f600
Anthony Hernandez <anthonyh2020@gmail.com>
2020-08-29 05:53:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,5 @@
-def reverse_each_word_with_each(strings)
-  array = strings.split(" ")
-  new_array = []
-  array.each do |string|
-    new_array << strings.reverse 
-  end
-  new_array.join(" ")
-end
-
-def reverse_each_word(strings)
-  an_array = strings.split
-  another_array = []
-  an_array.collect do |string|
-    another_array << strings.reverse
-  end
-  another_array.join(" ")
+def reverse_each_word(arrays)
+  new_array = arrays.split(" ")
+  reversed_array = new_array.each {|array| array.reverse!}
+  return reversed_array.join(" ")
 end
